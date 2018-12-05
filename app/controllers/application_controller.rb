6f5d0cec45c9017fabc8efb3de5c93b3263e7a77
TomKo1 <kottomasz98@gmail.com>
2018-12-05 11:40:46 +0100

    Devise config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    # additional config for params in other views reference - devise docs
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nick])
+    end
 end
